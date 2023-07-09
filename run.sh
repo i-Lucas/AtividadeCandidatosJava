223b05532b8359456e572a539af345573ae2d35d
i-Lucas <novo.contato.lucas@gmail.com>
2023-07-09 17:24:31 -0300

    fix: make sure maven is installed correctly

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! command -v mvn &> /dev/null; then
+  echo "Erro: Maven não encontrado. Certifique-se de ter o Maven instalado no seu sistema."
+  exit 1
+fi
+
 if [ "$1" = "-test" ]; then
   mvn test
 else
